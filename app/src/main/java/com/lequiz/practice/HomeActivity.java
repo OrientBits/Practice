9710ac436cb5da396f54bd416c03470803cc9fad
Ramshek Rana <ramshekrana@gamil.com>
2018-06-28 18:52:36 +0530

    adding main menu by ramshek rana

--- app/src/main/java/com/lequiz/practice/HomeActivity.java
+++ app/src/main/java/com/lequiz/practice/HomeActivity.java
@@ -1,35 +1,60 @@
 package com.lequiz.practice;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
 
 public class HomeActivity extends AppCompatActivity {
-    private CardView currentAffairs,computer,mathematics,reasoning,chemistry,
-            physics,biology,geography,history,entertainment;
+
+    protected DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    Toolbar toolbar;
+
+    protected CardView currentAffairs, computer, mathematics, reasoning, science,
+            english, geography, history, technology, sport, special, entertainment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         currentAffairs = findViewById(R.id.current_affairs_card_view);
         computer = findViewById(R.id.computer_card_view);
         mathematics = findViewById(R.id.mathematics_card_view);
         reasoning = findViewById(R.id.reasoning_card_view);
-        chemistry = findViewById(R.id.chemistry_card_view);
-        physics = findViewById(R.id.physics_card_view);
-        biology = findViewById(R.id.biology_card_view);
+        science = findViewById(R.id.science_card_view);
+        english = findViewById(R.id.english_card_view);
         geography = findViewById(R.id.geography_card_view);
         history = findViewById(R.id.history_card_view);
+        technology = findViewById(R.id.technology_card_view);
+        sport = findViewById(R.id.sport_card_view);
+        special = findViewById(R.id.special_card_view);
         entertainment = findViewById(R.id.entertainment_card_view);
 
         currentAffairs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,CurrentAffairsActivity.class);
+                Intent intent = new Intent(HomeActivity.this, CurrentAffairsActivity.class);
                 startActivity(intent);
             }
         });
@@ -37,7 +62,7 @@ public class HomeActivity extends AppCompatActivity {
         computer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,ComputerActivity.class);
+                Intent intent = new Intent(HomeActivity.this, ComputerActivity.class);
                 startActivity(intent);
             }
         });
@@ -45,7 +70,7 @@ public class HomeActivity extends AppCompatActivity {
         mathematics.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,MathematicsActivity.class);
+                Intent intent = new Intent(HomeActivity.this, MathematicsActivity.class);
                 startActivity(intent);
             }
         });
@@ -53,31 +78,24 @@ public class HomeActivity extends AppCompatActivity {
         reasoning.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,ReasoningActivity.class);
+                Intent intent = new Intent(HomeActivity.this, ReasoningActivity.class);
                 startActivity(intent);
             }
         });
 
-        chemistry.setOnClickListener(new View.OnClickListener() {
+        science.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,ChemistryActivity.class);
+                Intent intent = new Intent(HomeActivity.this, ScienceActivity.class);
                 startActivity(intent);
             }
         });
 
-        physics.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,PhysicsActivity.class);
-                startActivity(intent);
-            }
-        });
 
-        biology.setOnClickListener(new View.OnClickListener() {
+        english.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,BiologyActivity.class);
+                Intent intent = new Intent(HomeActivity.this, EnglishActivity.class);
                 startActivity(intent);
             }
         });
@@ -85,7 +103,7 @@ public class HomeActivity extends AppCompatActivity {
         geography.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,GeographyActivity.class);
+                Intent intent = new Intent(HomeActivity.this, GeographyActivity.class);
                 startActivity(intent);
             }
         });
@@ -93,7 +111,33 @@ public class HomeActivity extends AppCompatActivity {
         history.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,HistoryActivity.class);
+                Intent intent = new Intent(HomeActivity.this, HistoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        technology.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, TechnologyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        sport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, SportActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        special.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, SpecialActivity.class);
                 startActivity(intent);
             }
         });
@@ -101,9 +145,56 @@ public class HomeActivity extends AppCompatActivity {
         entertainment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,EntertainmentActivity.class);
+                Intent intent = new Intent(HomeActivity.this, EntertainmentActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int res_if = item.getItemId();
+        switch (res_if)
+        {
+            case R.id.search:
+                Toast.makeText(getApplicationContext(), "Search", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.profile:
+                Toast.makeText(getApplicationContext(), "Profile", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.notifications:
+                Toast.makeText(getApplicationContext(), "Notifications", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.leaderboard:
+                Toast.makeText(getApplicationContext(), "LeaderBoard", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.lesson_factory:
+                Toast.makeText(getApplicationContext(), "Lesson Factory", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.quiz_factory:
+                Toast.makeText(getApplicationContext(), "Quiz Factory", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.invite_friends:
+                Toast.makeText(getApplicationContext(), "Invite Friends", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.rate:
+                Toast.makeText(getApplicationContext(), "Rate us", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_LONG).show();
+                break;
+        }
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return true;
     }
 }
