006704e0b0d028b0e6dc92200b1d47807b25da8e
Ramshek Rana <ramshekrana@gamil.com>
2018-07-02 17:27:16 +0530

    almost final of cardView

--- app/src/main/java/com/lequiz/practice/HomeActivity.java
+++ app/src/main/java/com/lequiz/practice/HomeActivity.java
@@ -151,6 +151,9 @@ public class HomeActivity extends AppCompatActivity {
                             case R.id.help:
                                 Toast.makeText(getApplicationContext(), "Help", Toast.LENGTH_LONG).show();
                                 break;
+                            case R.id.about_us:
+                                Toast.makeText(getApplicationContext(), "About us", Toast.LENGTH_LONG).show();
+                                break;
                         }
 
 
