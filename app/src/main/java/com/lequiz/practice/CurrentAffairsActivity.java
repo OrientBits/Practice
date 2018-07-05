148f86546e9a3e4eeba78f8a436bdbc4a839e194
Ramshek Rana <ramshekrana@gamil.com>
2018-07-05 06:46:57 +0530

    adding GridView by xml

--- app/src/main/java/com/lequiz/practice/CurrentAffairsActivity.java
+++ app/src/main/java/com/lequiz/practice/CurrentAffairsActivity.java
@@ -3,8 +3,6 @@ package com.lequiz.practice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.widget.GridView;
-import java.util.ArrayList;
 
 public class CurrentAffairsActivity extends AppCompatActivity {
 
@@ -12,26 +10,10 @@ public class CurrentAffairsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.category_list);
+        setContentView(R.layout.activity_current_affairs);
 
         toolbar = findViewById(R.id.category_toolbar);
         setSupportActionBar(toolbar);
 
-
-        ArrayList<Word> arrayList = new ArrayList<>();
-        arrayList.add(new Word("Current Affairs",R.drawable.current_affairs,R.color.current_affairs));
-        arrayList.add(new Word("Computer",R.drawable.computer,R.color.computer));
-        arrayList.add(new Word("Mathematics",R.drawable.mathematics,R.color.mathematics));
-        arrayList.add(new Word("Reasoning",R.drawable.reasoning,R.color.reasoning));
-        arrayList.add(new Word("General Science",R.drawable.science,R.color.general_science));
-        arrayList.add(new Word("English",R.drawable.english,R.color.english));
-        arrayList.add(new Word("Technology",R.drawable.technology,R.color.technology));
-        arrayList.add(new Word("Sport",R.drawable.sport,R.color.sport));
-        arrayList.add(new Word("Special",R.drawable.special,R.color.special));
-        arrayList.add(new Word("Entertainment",R.drawable.entertainment,R.color.entertainment));
-
-        WordAdapter wordAdapter = new WordAdapter(this,arrayList);
-        GridView gridView = findViewById(R.id.list);
-        gridView.setAdapter(wordAdapter);
     }
 }
--- app/src/main/java/com/lequiz/practice/HomeActivity.java
+++ app/src/main/java/com/lequiz/practice/HomeActivity.java
@@ -13,10 +13,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.GridView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -30,12 +29,11 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.category_list);
+        setContentView(R.layout.activity_home);
 
         toolbar = findViewById(R.id.category_toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Home");
-
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Home");
 
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
@@ -44,35 +42,15 @@ public class HomeActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
         NavigationView navigationView =  findViewById(R.id.nav_view);
         navigationView.setItemIconTintList(null);
-
-            setupDrawerContent(navigationView); // default true
-
-
-        ArrayList<Word> arrayList = new ArrayList<>();
-        arrayList.add(new Word("Current Affairs", R.drawable.current_affairs,R.color.current_affairs));
-        arrayList.add(new Word("Computer", R.drawable.computer,R.color.computer));
-        arrayList.add(new Word("Mathematics", R.drawable.mathematics,R.color.mathematics));
-        arrayList.add(new Word("Reasoning", R.drawable.reasoning,R.color.reasoning));
-        arrayList.add(new Word("General Science", R.drawable.science,R.color.general_science));
-        arrayList.add(new Word("English", R.drawable.english,R.color.english));
-        arrayList.add(new Word("Technology", R.drawable.technology,R.color.technology));
-        arrayList.add(new Word("Sports", R.drawable.sport,R.color.sport));
-        arrayList.add(new Word("Special", R.drawable.special,R.color.special));
-        arrayList.add(new Word("Entertainment", R.drawable.entertainment,R.color.entertainment));
-
-        WordAdapter wordAdapter = new WordAdapter(this, arrayList);
-        GridView gridView = findViewById(R.id.list);
-        gridView.setAdapter(wordAdapter);
-
-
+        setupDrawerContent(navigationView); // default true
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -118,8 +96,7 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void setupDrawerContent(NavigationView navigationView) {
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
+        navigationView.setNavigationItemSelectedListener( new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                         int res_id = menuItem.getItemId();
