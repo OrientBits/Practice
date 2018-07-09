39a67ce3529806bcd70b40c81e8f56d934cd5b53
Ramshek Rana <ramshekrana@gamil.com>
2018-07-10 00:13:26 +0530

    added dialog box exit or not in HomeActivity and also drawer layout is open or closed if open the first it should be closed and then after dialog box appear

--- app/src/main/java/com/lequiz/practice/HomeActivity.java
+++ app/src/main/java/com/lequiz/practice/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.lequiz.practice;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.LinearGradient;
@@ -8,17 +9,21 @@ import android.graphics.Shader;
 
 import android.net.Uri;
 import android.os.Build;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -53,20 +58,19 @@ public class HomeActivity extends AppCompatActivity {
 
         // user name on home page gradient
         TextView txt = findViewById(R.id.user_name);
-        Shader textShader=new LinearGradient(0,0, 180, 0,
-                new int[]{getResources().getColor(R.color.blueOnHomeText),getResources().getColor(R.color.purpleOnHomeText)},
+        Shader textShader = new LinearGradient(0, 0, 180, 0,
+                new int[]{getResources().getColor(R.color.blueOnHomeText), getResources().getColor(R.color.purpleOnHomeText)},
                 new float[]{0, 1}, Shader.TileMode.CLAMP);
         txt.getPaint().setShader(textShader);
 
         TextView txt1 = findViewById(R.id.wishing);
-        Shader textShader1=new LinearGradient(0,0, 180, 0,
-                new int[]{getResources().getColor(R.color.blueOnHomeText),getResources().getColor(R.color.purpleOnHomeText)},
+        Shader textShader1 = new LinearGradient(0, 0, 180, 0,
+                new int[]{getResources().getColor(R.color.blueOnHomeText), getResources().getColor(R.color.purpleOnHomeText)},
                 new float[]{0, 1}, Shader.TileMode.CLAMP);
         txt1.getPaint().setShader(textShader1);
 
 
-
-    // open drawer when navigation button is tappeed
+        // open drawer when navigation button is tappeed
         mDrawerLayout = findViewById(R.id.drawer_layout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
@@ -96,79 +100,109 @@ public class HomeActivity extends AppCompatActivity {
         currentAffairs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,CurrentAffairsActivity.class);
+                Intent intent = new Intent(HomeActivity.this, CurrentAffairsActivity.class);
                 startActivity(intent);
             }
         });
         computer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,ComputerActivity.class);
+                Intent intent = new Intent(HomeActivity.this, ComputerActivity.class);
                 startActivity(intent);
             }
         });
         mathematics.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,MathematicsActivity.class);
+                Intent intent = new Intent(HomeActivity.this, MathematicsActivity.class);
                 startActivity(intent);
             }
         });
         reasoning.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,ReasoningActivity.class);
+                Intent intent = new Intent(HomeActivity.this, ReasoningActivity.class);
                 startActivity(intent);
             }
         });
         generalScience.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,GeneralScienceActivity.class);
+                Intent intent = new Intent(HomeActivity.this, GeneralScienceActivity.class);
                 startActivity(intent);
             }
         });
         english.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,EnglishActivity.class);
+                Intent intent = new Intent(HomeActivity.this, EnglishActivity.class);
                 startActivity(intent);
             }
         });
         technology.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,TechnologyActivity.class);
+                Intent intent = new Intent(HomeActivity.this, TechnologyActivity.class);
                 startActivity(intent);
             }
         });
         sports.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,SportsActivity.class);
+                Intent intent = new Intent(HomeActivity.this, SportsActivity.class);
                 startActivity(intent);
             }
         });
         special.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,SpecialActivity.class);
+                Intent intent = new Intent(HomeActivity.this, SpecialActivity.class);
                 startActivity(intent);
             }
         });
         entertainment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,EntertainmentActivity.class);
+                Intent intent = new Intent(HomeActivity.this, EntertainmentActivity.class);
                 startActivity(intent);
             }
         });
 
 
-
-
-
     } // onCreate method
+
+
+    //show a dialog message when click back button exit of nor also drawer layout closed or not
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+            } else {
+                exitByBackKey();
+            }
+            //moveTaskToBack(false);
+
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    protected void exitByBackKey() {
+        AlertDialog alertbox = new AlertDialog.Builder(this)
+                .setMessage("Do you want to exit application?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    // do something when the button is clicked
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        finish();
+                        //close();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    // do something when the button is clicked
+                    public void onClick(DialogInterface arg0, int arg1) {
+                    }
+                }).show();
+    }
 
 
     @Override
@@ -258,24 +292,24 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
-    public void makeFeedBackIntent()
-    {
+
+    public void makeFeedBackIntent() {
         String osVersion = Build.VERSION.RELEASE;
         Intent chooser;
         String locale = getResources().getConfiguration().locale.getDisplayCountry();
-        String manufacturerAndModal = Build.MANUFACTURER+" : "+Build.MODEL;
-        String msgtxt = "-----------------------------------------------------"+"\n";
-        msgtxt = msgtxt +"Support Diagnostics (Do Not Delete)"+"\n"+"-----------------------------------------------------"+"\n";
-        msgtxt = msgtxt +"U: "+getString(R.string.user_name)+"\n"+"V: "+osVersion+"\n"+"M: "+manufacturerAndModal+"\n"+"S: "+Build.VERSION.SDK_INT+"\n"+"G: "+locale+"\n";
-        msgtxt = msgtxt +"-----------------------------------------------------"+"\n\n";
+        String manufacturerAndModal = Build.MANUFACTURER + " : " + Build.MODEL;
+        String msgtxt = "-----------------------------------------------------" + "\n";
+        msgtxt = msgtxt + "Support Diagnostics (Do Not Delete)" + "\n" + "-----------------------------------------------------" + "\n";
+        msgtxt = msgtxt + "U: " + getString(R.string.user_name) + "\n" + "V: " + osVersion + "\n" + "M: " + manufacturerAndModal + "\n" + "S: " + Build.VERSION.SDK_INT + "\n" + "G: " + locale + "\n";
+        msgtxt = msgtxt + "-----------------------------------------------------" + "\n\n";
         Intent feedbackIntent = new Intent(Intent.ACTION_SEND);
         feedbackIntent.setData(Uri.parse("mailto:"));
-        String []to={"feedback@lequiz.com"};
+        String[] to = {"feedback@lequiz.com"};
         feedbackIntent.putExtra(Intent.EXTRA_EMAIL, to);
-        feedbackIntent.putExtra(Intent.EXTRA_SUBJECT,"FEEDBACK FROM PRECIOUS USERS");
-        feedbackIntent.putExtra(Intent.EXTRA_TEXT,msgtxt);
+        feedbackIntent.putExtra(Intent.EXTRA_SUBJECT, "FEEDBACK FROM PRECIOUS USERS");
+        feedbackIntent.putExtra(Intent.EXTRA_TEXT, msgtxt);
         feedbackIntent.setType("message/rfc822");
-        chooser = Intent.createChooser(feedbackIntent,"Send Feedback Through Email");
+        chooser = Intent.createChooser(feedbackIntent, "Send Feedback Through Email");
         startActivity(chooser);
     }
 
