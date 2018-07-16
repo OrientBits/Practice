2436e0bd52d840901d1dd9341c7697f300389f0d
Rishabh <rishabh.tsa@gmail.com>
2018-07-16 06:28:44 +0530

    Video Splash added

--- app/src/main/java/com/lequiz/practice/SplashActivity.java
+++ app/src/main/java/com/lequiz/practice/SplashActivity.java
@@ -1,17 +1,38 @@
 package com.lequiz.practice;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
+
+import com.felipecsl.gifimageview.library.GifImageView;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class SplashActivity extends AppCompatActivity {
 
-    public static int SPLASH_TIME_OUT=1500; // 1.5 second
+    public static int SPLASH_TIME_OUT=2855;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        GifImageView gifImageView = (GifImageView) findViewById(R.id.splash_gif_id);
+        try
+        {
+            InputStream inputStream = getAssets().open("splash.gif");
+            byte [] bytes = IOUtils.toByteArray(inputStream);
+            gifImageView.setBytes(bytes);
+            gifImageView.startAnimation();
+        }
+        catch (IOException ex)
+        {
+
+        }
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
