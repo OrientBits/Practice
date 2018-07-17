dd135a99e4e74ea80c9d0dff69eab28532f939e9
Rishabh <37104253+Rexrazor@users.noreply.github.com>
2018-07-17 13:36:35 +0530

    Profile Activity Java Fix 0

--- app/src/main/java/com/lequiz/practice/ProfileActivity.java
+++ app/src/main/java/com/lequiz/practice/ProfileActivity.java
@@ -22,6 +22,6 @@ public class ProfileActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setTitle("Porfile");
+       
     }
 }
