28c64b856d61fe194039eec95d25193c470ee54d
Rishabh <rishabh.tsa@gmail.com>
2018-07-21 13:33:36 +0530

    Setting Menu Implemented and Splash Screen Fixed

--- app/src/main/java/com/lequiz/practice/NavSettings.java
+++ app/src/main/java/com/lequiz/practice/NavSettings.java
@@ -1,14 +1,21 @@
 package com.lequiz.practice;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Switch;
 
 import java.util.Objects;
 
 public class NavSettings extends AppCompatActivity {
 
     protected Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +25,11 @@ public class NavSettings extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_button_settings);
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.greenOnToolBarSettings)));
+
+
+
     }
 }
