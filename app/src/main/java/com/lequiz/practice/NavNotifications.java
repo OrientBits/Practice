7f4f01e9a3ba88961584c68e7a27626a47be5dcf
Ramshek Rana <ramshekrana@gamil.com>
2018-07-22 21:35:09 +0530

    almost done all drawer options many thing are fixed and don't change Rishi theme like status and toolbar color please

--- app/src/main/java/com/lequiz/practice/NavNotifications.java
+++ app/src/main/java/com/lequiz/practice/NavNotifications.java
@@ -1,8 +1,11 @@
 package com.lequiz.practice;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
 
 import java.util.Objects;
 
@@ -18,5 +21,11 @@ public class NavNotifications extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        // for status bar color
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(NavNotifications.this,R.color.colorPrimaryDark));
     }
 }
