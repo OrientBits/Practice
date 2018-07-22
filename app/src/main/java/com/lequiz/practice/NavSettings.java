c396808d32a54f04879198306f89448702bc453c
Ramshek Rana <ramshekrana@gamil.com>
2018-07-22 20:04:45 +0530

    Edit profile and lot of changes are fixed

--- app/src/main/java/com/lequiz/practice/NavSettings.java
+++ app/src/main/java/com/lequiz/practice/NavSettings.java
@@ -1,22 +1,24 @@
 package com.lequiz.practice;
 
-import android.annotation.SuppressLint;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
+
+import com.hbb20.CountryCodePicker;
+
 import java.util.Objects;
 
 public class NavSettings extends AppCompatActivity {
-    AlertDialog alertDialogRadioButtons;
 
 
     protected Toolbar toolbar;
@@ -31,7 +33,6 @@ public class NavSettings extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        // Edit Profile Click Event
         TextView edit_profile_on_settings=findViewById(R.id.edit_profile_on_settings);
         edit_profile_on_settings.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,91 +41,16 @@ public class NavSettings extends AppCompatActivity {
                 startActivity(intentEditProfile);
             }
         });
-        // Night Mode Click Event
-        LinearLayout night_mode_text_view = findViewById(R.id.night_mode_text_view);
-        night_mode_text_view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                createAlertDialogWithRadioButton();
-            }
-        });
-        // Language Click Event
-        LinearLayout language_text_view = findViewById(R.id.language_text_view);
-       language_text_view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                createAlertDialogWithRadioButtonForLanguage();
-            }
-        });
-
 
 
         // for status bar color
         Window window = getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(ContextCompat.getColor(NavSettings.this,R.color.darkGreenOnToolBarSettings));
+        window.setStatusBarColor(ContextCompat.getColor(NavSettings.this,R.color.colorPrimaryDark));
 
 
-    }
-    public void createAlertDialogWithRadioButton()
-        {
-            CharSequence[] value = {"On","Off"};
-            final AlertDialog.Builder builder = new AlertDialog.Builder(NavSettings.this);
-            builder.setTitle("Night Mode");
-            builder.setSingleChoiceItems(value, -1, new DialogInterface.OnClickListener() {
-                        @SuppressLint("SetTextI18n")
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            switch (i)
-                            {
-                                case 0:
-                                    TextView txtOn = findViewById(R.id.on_off_textView);
-                                    txtOn.setText("On");
-                                    break;
-                                case 1:
-                                    TextView txtOff = findViewById(R.id.on_off_textView);
-                                    txtOff.setText("Off");
-                                    break;
-                            }
-                            alertDialogRadioButtons.dismiss();
-                        }
 
-                    }
-
-            );
-            alertDialogRadioButtons = builder.create();
-            alertDialogRadioButtons.show();
-
-    }
-    public void createAlertDialogWithRadioButtonForLanguage()
-    {
-        CharSequence[] value = {"English","Hindi"};
-        final AlertDialog.Builder builder = new AlertDialog.Builder(NavSettings.this);
-        builder.setTitle("Language");
-        builder.setSingleChoiceItems(value, -1, new DialogInterface.OnClickListener() {
-                    @SuppressLint("SetTextI18n")
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        switch (i)
-                        {
-                            case 0:
-                                TextView txtOn = findViewById(R.id.hindi_english_textview);
-                                txtOn.setText("English");
-                                break;
-                            case 1:
-                                TextView txtOff = findViewById(R.id.hindi_english_textview);
-                                txtOff.setText("Hindi");
-                                break;
-                        }
-                        alertDialogRadioButtons.dismiss();
-                    }
-
-                }
-
-        );
-        alertDialogRadioButtons = builder.create();
-        alertDialogRadioButtons.show();
 
     }
 }
