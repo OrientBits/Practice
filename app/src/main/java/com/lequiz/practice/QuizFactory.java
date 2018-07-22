3e76b14427634b23d97119d44cc1502f7c374aac
Ramshek Rana <ramshekrana@gamil.com>
2018-07-22 20:04:45 +0530

    Edit profile and lot of changes are fixed

--- app/src/main/java/com/lequiz/practice/QuizFactory.java
+++ app/src/main/java/com/lequiz/practice/QuizFactory.java
@@ -1,11 +1,8 @@
 package com.lequiz.practice;
 
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Window;
-import android.view.WindowManager;
 
 import java.util.Objects;
 
@@ -22,12 +19,6 @@ public class QuizFactory extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        // for status bar color
-        Window window = getWindow();
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(ContextCompat.getColor(QuizFactory.this,R.color.colorPrimaryDark));
 
     }
 }
