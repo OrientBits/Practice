68bc04bf283cb3b63a7d7dd48a8320a9b5f0db62
Ramshek Rana <ramshekrana@gamil.com>
2018-07-23 01:13:46 +0530

    almost done all drawer options many thing are fixed and don't change Rishi theme like status and toolbar color please now

--- app/src/main/java/com/lequiz/practice/EditProfileActivity.java
+++ app/src/main/java/com/lequiz/practice/EditProfileActivity.java
@@ -28,7 +28,7 @@ public class EditProfileActivity extends AppCompatActivity {
        Window window = getWindow();
        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-       window.setStatusBarColor(ContextCompat.getColor(EditProfileActivity.this,R.color.colorPrimaryDark));
+       window.setStatusBarColor(ContextCompat.getColor(EditProfileActivity.this,R.color.darkGreenOnToolBarSettings));
 
 //       TextView textViewChangeAvatar = findViewById(R.id.changeAvatar);
        final EditText editTextOfLocationOnEditProfile = findViewById(R.id.location_edit_text_on_profile);
