f28b4611db07c77e6219f761b6636d4ec02a7940
Ramshek Rana <ramshekrana@gamil.com>
2018-07-23 21:49:06 +0530

    choose category is added in spinner of quiz factory

--- app/src/main/java/com/lequiz/practice/QuizFactory.java
+++ app/src/main/java/com/lequiz/practice/QuizFactory.java
@@ -4,14 +4,20 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.ArrayList;
 import java.util.Objects;
 
-public class QuizFactory extends AppCompatActivity {
+public class QuizFactory extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     protected Toolbar toolbar;
+    protected Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +34,49 @@ public class QuizFactory extends AppCompatActivity {
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(ContextCompat.getColor(QuizFactory.this,R.color.darkGreenOnToolBarSettings));
+
+
+//        for spinner chooser
+        spinner = findViewById(R.id.spinner_to_choose_category);
+
+        spinner.setOnItemSelectedListener(this);
+
+        // spinner drop down elements or categories
+        ArrayList<String> categorySelecter = new ArrayList<>();
+        categorySelecter.add("Choose Category");
+        categorySelecter.add("Current Affairs");
+        categorySelecter.add("Computer");
+        categorySelecter.add("Mathematics");
+        categorySelecter.add("Reasoning");
+        categorySelecter.add("General Science");
+        categorySelecter.add("English");
+        categorySelecter.add("Technology");
+        categorySelecter.add("Sports");
+        categorySelecter.add("Special");
+        categorySelecter.add("Entertainment");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categorySelecter);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        // here as item action to be performed
+
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
 }
