55bda006038687bb5ccfb053bcea29118b83fb37
Ramshek Rana <ramshekrana@gamil.com>
2018-09-14 15:21:39 +0530

    something is fixed...

--- app/src/main/java/com/lequiz/practice/ComputerActivity.java
+++ app/src/main/java/com/lequiz/practice/ComputerActivity.java
@@ -1,21 +1,20 @@
 package com.lequiz.practice;
 
-import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.LinearGradient;
 import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
 
 import java.util.Objects;
 
 public class ComputerActivity extends AppCompatActivity {
 
-     Toolbar toolbar;
-
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +26,13 @@ public class ComputerActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_button_computer);
+
+        // Size troubleshoot Learn Section
+        Drawable drawable= getResources().getDrawable(R.drawable.back_button_computer);
+        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+        Drawable newdrawable = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap, 130, 130, true));
+
+        getSupportActionBar().setHomeAsUpIndicator(newdrawable);
 
         // Gradient Heading
         // Heading TextView gradient
@@ -38,23 +43,10 @@ public class ComputerActivity extends AppCompatActivity {
                 new float[]{0, 1}, Shader.TileMode.CLAMP);
         learnHeaderTech.getPaint().setShader(textShader);
 
-        // Hey UserName Initialization on learn section
-//
-//        TextView heyUserName = findViewById(R.id.hey_user_name);
-//        String heyUserNameMaker = "Hey "+getString(R.string.user_first_name)+",";
-//        heyUserName.setText(heyUserNameMaker);
+        // Hey UserName Initilization on learn section
 
-
-
-         CardView startQuiz = findViewById(R.id.start_CardView_of_computer_quiz);
-         startQuiz.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 startActivity(new Intent(ComputerActivity.this,ComputerQuiz.class));
-             }
-         });
+        TextView heyUserName = findViewById(R.id.hey_user_name);
+        String heyUserNameMaker = "Hey "+getString(R.string.user_first_name)+",";
+        heyUserName.setText(heyUserNameMaker);
     }
-
-
-
 }
