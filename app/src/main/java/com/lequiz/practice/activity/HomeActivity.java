07ee2bd8b4a48578f0af925220fc90e8185dcd76
Rishabh <rishabh.tsa@gmail.com>
2018-10-28 10:07:36 +0530

    Firebase is connected to our app.......

--- app/src/main/java/com/lequiz/practice/activity/HomeActivity.java
+++ app/src/main/java/com/lequiz/practice/activity/HomeActivity.java
@@ -34,6 +34,12 @@ import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
 import com.github.ksoichiro.android.observablescrollview.ScrollState;
 import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.lequiz.practice.base.FullScreenStatusOnly;
 import com.lequiz.practice.nav_drawer.NavInviteFriends;
 import com.lequiz.practice.nav_drawer.NavLeaderboard;
@@ -66,6 +72,9 @@ public class HomeActivity extends AppCompatActivity implements ObservableScrollV
     @SuppressLint("StaticFieldLeak")
     public static Activity fa; // finish activity
     public MenuItem menuItem;
+
+    // Testing firebase
+    DatabaseReference mRefRoot;
 
 
 
@@ -348,8 +357,6 @@ public class HomeActivity extends AppCompatActivity implements ObservableScrollV
 
             }
         });
-
-
 
     }
 
