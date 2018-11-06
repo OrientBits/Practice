534f638405bbbea009e81440a6edaca5fbcd7e0d
Rishabh <rishabh.tsa@gmail.com>
2018-11-06 12:39:53 +0530

    Pressing the back button doest goes to login or register

--- app/src/main/java/com/lequiz/practice/activity/Login.java
+++ app/src/main/java/com/lequiz/practice/activity/Login.java
@@ -104,5 +104,6 @@ public class Login extends AppCompatActivity {
     public void createAccount(View view) {
         startActivity(new Intent(this,RegisterUser.class));
         finish();
+
     }
 }
