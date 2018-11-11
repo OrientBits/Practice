913f8cfdf840f40dd5e2e6727134d278f8175a16
Rishabh <rishabh.tsa@gmail.com>
2018-11-12 04:44:16 +0530

    Added activity to choose gender

--- app/src/main/java/com/lequiz/practice/nav_drawer/ProfileActivity.java
+++ app/src/main/java/com/lequiz/practice/nav_drawer/ProfileActivity.java
@@ -199,7 +199,6 @@ public class ProfileActivity extends AppCompatActivity implements ObservableScro
 
 
 
-
         // Finding profile section main image variable
 
         profileImageCardView = findViewById(R.id.profile_image_card_view);
