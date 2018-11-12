d76655cd6dd9de8440648fcd2926d7c7f21c645f
Rishabh <rishabh.tsa@gmail.com>
2018-11-12 10:11:27 +0530

    Cleared cache while signing out..

--- app/src/main/java/com/lequiz/practice/nav_drawer/NavSettings.java
+++ app/src/main/java/com/lequiz/practice/nav_drawer/NavSettings.java
@@ -157,6 +157,5 @@ public class NavSettings extends AppCompatActivity {
         startActivity(new Intent(this,Login.class));
         finish();
         HomeContainer.fa.finish();
-
-    }
+        }
 }
